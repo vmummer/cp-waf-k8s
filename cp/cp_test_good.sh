3d156b7f1e959d4015add7ea07c2c44e66eff55d
Vince Mammoliti <vince@homepage.ca>
2025-04-21 20:55:44 -0400

    updates

--- cp/cp_test_good.sh
+++ cp/cp_test_good.sh
@@ -1,21 +1,62 @@
 #/usr/bin/bash
-DOCKER_HOST="`hostname -I| awk ' {print $1}'`"
-if [ -z "$1" ]; then
-        echo "Usage  cptrgood <URL> <repeat>  - Create Good Traffic against Test Host. " 
-	echo "Defaulting to Test Host URL:  http://juiceshop.local:80 and repeat 1"
-	host="http://juiceshop.local:80"
-	repeat=1
-else
-	host=$1
-	repeat=$2
-	if [ -z "$repeat" ]; then repeat=1 ;fi 
-fi
+# April 21 2025 - Modified to run in K8S pod.
+# Written by Vince Mammoliti - vincem@checkpoint.com
 
-for (( i=0; i<$repeat; ++i)); do
-	loop=$(($i+1))
-	echo "$loop of $repeat) Testing Against URL: $host"
-	docker run -it --rm -v $(pwd)/data:/home/web-scraper/data --add-host juiceshop.local:$DOCKER_HOST appsec-demo-test-host python /home/web-scraper/websitescrap.py $host
+HOST="http://juiceshop.lab"
+REPEAT=1
 
+
+usage(){
+	>&2 cat << EOF
+$0 is traffic generator to create non malicious web traffic against a Web Test Host
+Scripted by Vince Mammoliti - vincem@checkpoint.com - April 2025
+
+Usage: $0 [OPTIONS....] URL of Web Host - defaults to $HOST]
+  -r | --repeat          repeat the number of times to crawl the Web Host
+  -h | --help           this help screen is displayed
+
+EOF
+exit 1
+}
+
+
+
+# Start of main script
+
+args=$(getopt -a -o vr:smi --long help,verbose,repeat:,sql,malicious,initdb -- "$@")
+
+eval set -- ${args}
+while :
+do
+	case $1 in
+		-h | --help)    usage	; shift ;;
+		-r | --repeat)  REPEAT=$2 ; shift 2 ;;
+		--) shift; break ;;
+		*)  usage; exit 1;;
+	esac
 
 done
 
+
+if [ ! -z "$@" ]; then     # Check to see if there is a URL on the command, if so replace
+		 HOST=$@
+fi
+
+
+echo "HOST: ${HOST}"
+
+
+
+
+
+
+
+
+
+
+
+for (( i=0; i<$REPEAT; ++i)); do
+	loop=$(($i+1))
+	echo "$loop of $REPEAT) Testing Against URL: $HOST"
+	python /home/web-scraper/websitescrap.py $HOST
+done
